3aed613356ff4f869bfe392d8477bdaf642752fe
AaryanPalve5 <aspalve2004@gmail.com>
2024-08-24 03:35:20 +0530

    n

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
-from flask import Flask,render_template,request,jsonify
+import nltk
+from flask import Flask, render_template, request, jsonify
 from nltk.tokenize import word_tokenize
 from chat import get_response
 from flask_cors import CORS
+
+nltk.download('punkt')
+
 app = Flask(__name__)
 CORS(app)
+
 @app.get("/")
 def index_get():
     return render_template("base.html")
 
 @app.post("/predict")
 def predict():
-    
-    
-    text=request.get_json().get("message")
-    #TODO :check if text is valid
+    text = request.get_json().get("message")
     if text is None or text.strip() == "":
         return jsonify({"answer": "Invalid input."})
     
-    response=get_response(text)
-    message = {"answer":response}
+    response = get_response(text)
+    message = {"answer": response}
     return jsonify(message)
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
